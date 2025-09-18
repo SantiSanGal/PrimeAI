4536cee439ea7675e91095f681bb49f8fa9b4441
Santiago Galvan <sngalvan@tu.com.py>
2025-09-18 09:41:09 -0300

    chore: massive cleanup, removed many files and applied project changes

--- src/core/types/items/categories.ts
+++ src/core/types/items/categories.ts
@@ -1,24 +1,24 @@
 // TODO: Decirle a Hugo que cambie el tipado
-export interface Categorie {
-    category: Category;
+export interface Categorieaaa {
+  category: Categoryaaa;
 }
 
-export interface Category {
-    content: Content[];
-    page: Page;
+export interface Categoryaaa {
+  content: Content[];
+  page: Page;
 }
 
 export interface Content {
-    id: string;
-    createdAt: Date;
-    updatedAt: Date;
-    enabled: boolean;
-    name: string;
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  enabled: boolean;
+  name: string;
 }
 
 export interface Page {
-    size: number;
-    number: number;
-    totalElements: number;
-    totalPages: number;
+  size: number;
+  number: number;
+  totalElements: number;
+  totalPages: number;
 }
--- src/sections/product/view/product-list-view.tsx
+++ src/sections/product/view/product-list-view.tsx
@@ -1,59 +1,51 @@
-import type { Theme, SxProps } from '@mui/material/styles';
-import type { UseSetStateReturn } from 'minimal-shared/hooks';
 import type { IProductItem, IProductTableFilters } from 'src/types/product';
-import type {
-  GridColDef,
-  GridSlotProps,
-  GridRowSelectionModel,
-  GridActionsCellItemProps,
-  GridColumnVisibilityModel,
-} from '@mui/x-data-grid';
-
-import { useBoolean, useSetState } from 'minimal-shared/hooks';
-import { useState, useEffect, forwardRef, useCallback } from 'react';
-
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import Card from '@mui/material/Card';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import {
-  DataGrid,
-  gridClasses,
-  GridToolbarExport,
-  GridActionsCellItem,
-  GridToolbarContainer,
-  GridToolbarQuickFilter,
-  GridToolbarFilterButton,
-  GridToolbarColumnsButton,
-} from '@mui/x-data-grid';
-
-import { paths } from 'src/routes/paths';
-import { RouterLink } from 'src/routes/components';
-
-import { PRODUCT_STOCK_OPTIONS } from 'src/_mock';
-import { useGetProducts } from 'src/actions/product';
-import { DashboardContent } from 'src/layouts/dashboard';
-
-import { toast } from 'src/components/snackbar';
-import { Iconify } from 'src/components/iconify';
-import { EmptyContent } from 'src/components/empty-content';
-import { ConfirmDialog } from 'src/components/custom-dialog';
-import { CustomBreadcrumbs } from 'src/components/custom-breadcrumbs';
-
-import { ProductTableToolbar } from '../product-table-toolbar';
 import { ProductTableFiltersResult } from '../product-table-filters-result';
+import { CustomBreadcrumbs } from 'src/components/custom-breadcrumbs';
+import { useState, useEffect, forwardRef, useCallback } from 'react';
+import { useBoolean, useSetState } from 'minimal-shared/hooks';
+import { ProductTableToolbar } from '../product-table-toolbar';
+import type { UseSetStateReturn } from 'minimal-shared/hooks';
+import { ConfirmDialog } from 'src/components/custom-dialog';
+import { EmptyContent } from 'src/components/empty-content';
+import type { Theme, SxProps } from '@mui/material/styles';
+import { DashboardContent } from 'src/layouts/dashboard';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import { useGetProducts } from 'src/actions/product';
+import { RouterLink } from 'src/routes/components';
+import { PRODUCT_STOCK_OPTIONS } from 'src/_mock';
+import { Iconify } from 'src/components/iconify';
+import { toast } from 'src/components/snackbar';
+import { useItems } from 'src/hooks/useItems';
+import MenuItem from '@mui/material/MenuItem';
+import { paths } from 'src/routes/paths';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
 import {
+  RenderCellCreatedAt,
+  RenderCellProduct,
+  RenderCellPublish,
   RenderCellStock,
   RenderCellPrice,
-  RenderCellPublish,
-  RenderCellProduct,
-  RenderCellCreatedAt,
 } from '../product-table-row';
-import { useItems } from 'src/hooks/useItems';
-
-// ----------------------------------------------------------------------
+import type {
+  GridColumnVisibilityModel,
+  GridActionsCellItemProps,
+  GridRowSelectionModel,
+  GridSlotProps,
+  GridColDef,
+} from '@mui/x-data-grid';
+import {
+  GridToolbarColumnsButton,
+  GridToolbarFilterButton,
+  GridToolbarQuickFilter,
+  GridToolbarContainer,
+  GridActionsCellItem,
+  GridToolbarExport,
+  gridClasses,
+  DataGrid,
+} from '@mui/x-data-grid';
 
 const PUBLISH_OPTIONS = [
   { value: 'published', label: 'Published' },
@@ -64,16 +56,14 @@ const HIDE_COLUMNS = { category: false };
 
 const HIDE_COLUMNS_TOGGLABLE = ['category', 'actions'];
 
-// ----------------------------------------------------------------------
-
 export function ProductListView() {
   const confirmDialog = useBoolean();
 
-  const { data } = useItems();
-  console.log('data', data);
-
   const { products, productsLoading } = useGetProducts();
-  // const { data: products, isloading: productsLoading } = useItems();
+  const { data, isLoading } = useItems();
+
+  console.log('data', data);
+  console.log('isLoading', isLoading);
 
   const [tableData, setTableData] = useState<IProductItem[]>(products);
   const [selectedRowIds, setSelectedRowIds] = useState<GridRowSelectionModel>([]);
